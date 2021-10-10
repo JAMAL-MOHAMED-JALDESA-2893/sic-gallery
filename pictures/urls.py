bfe83f96d4f154f088eb47b6095ddf97ccca900f
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-10 19:33:10 +0300

    add url patterns

--- pictures/urls.py
+++ pictures/urls.py
@@ -3,3 +3,13 @@ from django.urls import include,path,re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+
+urlpatterns=[
+    path('',views.pictures,name = 'pictures'),
+    re_path('location/(?P<location>\w+)',views.locations,name = 'location'),
+    path('search/', views.search_results, name='search_results')
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
