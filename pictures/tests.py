fd5cf578891e8fab601c67accf09e54f8f720b60
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-10 19:41:00 +0300

    created test for category model

--- pictures/tests.py
+++ pictures/tests.py
@@ -25,3 +25,23 @@ class TestLocation(TestCase):
         self.location.deleteLocation()
         location = Location.objects.all()
         self.assertTrue(len(location) == 0)
+
+
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.category = Category(name='food')
+        self.category.saveCategory()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_category(self):
+        self.category.saveCategory()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_category(self):
+        self.category.deleteCategory()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)        
