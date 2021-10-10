91097914359091aa38f47018223d4f1bf8520253
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-10 17:45:35 +0300

    added model class category

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class Category(models.Model):
+    categoryName = models.CharField(max_length=30)
