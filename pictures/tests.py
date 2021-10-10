7866641a093051169d0a7f98ff1004c25c2bd223
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-10 19:43:34 +0300

    adds test for class model image

--- pictures/tests.py
+++ pictures/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Image, Category, Location
+from __future__ import unicode_literals
 
 
 # Create your tests here.
@@ -44,4 +45,56 @@ class CategoryTestClass(TestCase):
     def test_delete_category(self):
         self.category.deleteCategory()
         category = Category.objects.all()
-        self.assertTrue(len(category) == 0)        
+        self.assertTrue(len(category) == 0)    
+
+
+
+class TestImage(TestCase):
+    def setUp(self):
+        self.location = Location(locationName='Kinoo')
+        self.location.saveLocation()
+
+        self.category = Category(categoryName='food')
+        self.category.saveCategory()
+
+        self.testInstance = Image(id=1, imageName='IMG__001', imageDescription=' a test image', imageLocation=self.location,
+                                imageCategory=self.category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.testInstance, Image))
+
+    def test_save_image(self):
+        self.testInstance.saveImage()
+        filterImage= Image.objects.all()
+        self.assertTrue(len(filterImage) > 0)
+
+    def test_delete_image(self):
+        self.testInstance.deleteImage()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def test_update_image(self):
+        self.testInstance.saveImage()
+        self.testInstance.updateImage(self.testInstance.id, 'images/img.jpg')
+        imgUpdt = Image.objects.filter(image='images/test.jpg')
+        self.assertTrue(len(imgUpdt) > 0)
+
+    def test_get_image_by_id(self):
+        imageF = self.testInstance.getimageById(self.testInstance.id)
+        image = Image.objects.filter(id=self.testInstance.id)
+        self.assertTrue(imageF, image)
+
+    def test_search_image_by_location(self):
+        self.testInstance.saveImage()
+        foundImages = self.testInstance.filterimageByLocation(imageLocation='kinoo')
+        self.assertTrue(len(found_images) == 1)
+
+    def test_search_image_by_category(self):
+        category = 'food'
+        foundImages = self.testInstance.searchImage(category)
+        self.assertTrue(len(found_img) > 1)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
