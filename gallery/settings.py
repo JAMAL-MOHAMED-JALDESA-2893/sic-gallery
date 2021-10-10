81ec524222fa39394d0851c876c6d01cbeb87fbc
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-10 19:31:59 +0300

    created view functions routes

--- gallery/settings.py
+++ gallery/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import cloudinary
 import os
 
 
@@ -21,7 +20,8 @@ import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
